0511f118a014fcf3dc4d94a02b245f3606ef01c5
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-03-17 06:37:21 +0530

    rouert

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => {
+  import("./pages/Home");
+});
+const Login = lazy(() => {
+  import("./pages/Login");
+});
+const Chat = lazy(() => {
+  import("./pages/Chat");
+});
+const Groups = lazy(() => {
+  import("./pages/Groups");
+});
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
